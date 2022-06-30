66a6f6565f74c4dbc33b87d536f9aae3a550ae20
georgemwangi118 <gmwangi118@yahoo.com>
2022-06-30 12:22:09 +0300

    added routing to the index.js file

--- server/index.js
+++ server/index.js
@@ -2,8 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+require("express-async-errors");
 
+//routes
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/songs");
+const playListRoutes = require("./routes/playLists");
+const searchRoutes = require("./routes/search");
+
+//app
 const app = express();
+
+//middleware
 app.use(express.json());
 app.use(cors());
 
@@ -14,6 +25,12 @@ mongoose
   })
   .then(() => console.log("Mongoose connected..."))
   .catch((err) => console.log(err));
+
+app.use("/api/users/", userRoutes);
+app.use("/api/login/", authRoutes);
+app.use("/api/songs/", songRoutes);
+app.use("/api/playlists/", playListRoutes);
+app.use("/api/", searchRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
