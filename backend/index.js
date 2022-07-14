428ddf6c43b0bb558000cf97230a0d7e02626a75
georgemwangi118 <gmwangi118@yahoo.com>
2022-07-15 00:39:50 +0300

    updated backend

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+//routes
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/songs");
+const playListRoutes = require("./routes/playLists");
+const searchRoutes = require("./routes/search");
+
+//app
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongoose connected..."))
+  .catch((err) => console.log(err));
+
+app.use("/api/users/", userRoutes);
+app.use("/api/login/", authRoutes);
+app.use("/api/songs/", songRoutes);
+app.use("/api/playlists/", playListRoutes);
+app.use("/api/", searchRoutes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
