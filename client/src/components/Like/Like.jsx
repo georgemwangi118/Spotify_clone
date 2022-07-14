520bf242fc3054fc26363e51715e61393d045d14
georgemwangi118 <gmwangi118@yahoo.com>
2022-07-14 21:12:44 +0300

    Like component

--- client/src/components/Like/Like.jsx
+++ client/src/components/Like/Like.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
+import { IconButton } from "@mui/material";
+import { FavoriteBorder, Favorite } from "@mui/icons-material";
 
 const Like = () => {
-  return <div>Like</div>;
+  const [like, setLike] = useState(false);
+
+  return (
+    <IconButton className={styles.like_btn} onClick={() => setLike(!like)}>
+      {!like ? (
+        <FavoriteBorder className={styles.like_outlined} />
+      ) : (
+        <Favorite className={styles.like_filled} />
+      )}
+    </IconButton>
+  );
 };
 
 export default Like;
