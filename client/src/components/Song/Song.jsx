630b48cc99b506d962ad3633831841ff81dc28ee
georgemwangi118 <gmwangi118@yahoo.com>
2022-07-14 21:32:58 +0300

    a song component

--- client/src/components/Song/Song.jsx
+++ client/src/components/Song/Song.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Like from "../Like";
+import { IconButton } from "@mui/material";
+import { PlayArrow, MoreHoriz } from "@mui/icons-material";
+import styles from "./styles.module.scss";
+import PlaylistMenu from "../PlaylistMenu/PlaylistMenu";
 
-const Song = () => {
-  return <div>Song</div>;
+const Song = ({ song, playlist }) => {
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <div className={styles.song_container}>
+      <div className={styles.left}>
+        <IconButton className={styles.play_btn}>
+          <PlayArrow />
+        </IconButton>
+        <img src={song.img} alt="song_img" />
+        <p>{song.name}</p>
+      </div>
+      <div className={styles.center}>
+        <p>{song.artist}</p>
+      </div>
+      <div className={styles.right}>
+        <Like songId={song._id} />
+        <p>4.30</p>
+        <IconButton className={styles.menu_btn} onClick={() => setMenu(true)}>
+          <MoreHoriz />
+        </IconButton>
+        {menu && (
+          <PlaylistMenu playlist={playlist} closeMenu={() => setMenu(false)} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Song;
