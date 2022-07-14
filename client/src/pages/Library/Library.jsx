4e69e67532295ff2c1bab5b6437d83ab4a1a3244
georgemwangi118 <gmwangi118@yahoo.com>
2022-07-14 21:38:42 +0300

    library page

--- client/src/pages/Library/Library.jsx
+++ client/src/pages/Library/Library.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import Playlists from "../../components/Playlists/Playlists";
+import playlistImg from "../../images/rock.jpg";
+import styles from "./styles.module.scss";
+
+const playlists = [
+  { _id: 1, img: playlistImg, name: "Today's Top Songs", desc: "By George" },
+];
 
 const Library = () => {
-  return <div>Library</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Playlists</h1>
+      <div className={styles.playlists_container}>
+        <Link to="/collection/tracks">
+          <div className={styles.liked_songs}>
+            <h1>Liked Songs</h1>
+            <p>1 Liked Songs</p>
+          </div>
+        </Link>
+        <Playlists playlists={playlists} />
+      </div>
+    </div>
+  );
 };
 
 export default Library;
